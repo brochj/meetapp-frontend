99f8d05241f7622e77f3a40501010384b6a12621
Oscar Broch <brochj@gmail.com>
2019-10-01 09:41:10 -0300

    Fix bug on Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 import { NButton } from './styles';
 
-export default function Button({ children, background, color }) {
+export default function Button({ children, background, color, ...props }) {
   return (
-    <NButton background={background} color={color}>
+    <NButton {...props} background={background} color={color}>
       {children}
     </NButton>
   );
